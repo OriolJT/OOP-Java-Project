7a2370e9d55229e8802759179b3d618f768f0744
orioljt <orioljt5@gmail.com>
2021-12-08 19:56:47 +0100

    basic functions for Game + board created

--- core/src/com/snake/game/Game.java
+++ core/src/com/snake/game/Game.java
@@ -6,8 +6,21 @@ public class Game {
     private Menu m_menu;
 
     void startGame(){
-        m_board.startGame();
+        m_board.startGame(this);
         m_screen.startGame();
         m_menu.startGame();
     }
+
+
+    /* Since we have to see the highscore in real time, instead of the screen asking for the highscore, the class
+        board will call the function setHighscore from the main so this one sends the order to the class Screen with
+        the new value of the highscore.*/
+    void setHighscore(int highscore){
+        m_screen.setHighscore(highscore);
+    }
+
+    void moveSnake(int player, int direction){
+        m_board.moveSnake(player, direction);
+    }
+
 }
