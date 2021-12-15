4688a69ba1716b5fe91010ab7788f4106c1ec82a
JoseCarioca <65904569+JoseCarioca@users.noreply.github.com>
2021-12-15 18:42:03 +0100

    Cell n snake

--- core/src/com/snake/game/Cell.java
+++ core/src/com/snake/game/Cell.java
@@ -1,4 +1,33 @@
 package com.snake.game;
 
 public class Cell {
+    private int x, y;
+    private State state; //what is inside the cell
+
+    public Cell(int x, int y){
+        this.x = x;
+        this y = y;
+    }
+
+    public State getState(){
+        return state;
+    }
+
+    public void setState(State state){
+        this.state = state;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
+
+    public enum State {
+        FREE,
+        SNAKE,
+        ITEM
+    }
 }
