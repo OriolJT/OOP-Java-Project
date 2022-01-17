f2d235ab392d2c8d57dffc94b0a32f3feb677b62
Skaze97 <77886530+Skaze97@users.noreply.github.com>
2022-01-17 02:40:03 +0100

    Few Fixes to HighscoreList functions

--- core/src/com/snake/game/HighscoreList.java
+++ core/src/com/snake/game/HighscoreList.java
@@ -1,14 +1,29 @@
 package com.snake.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
+import com.badlogic.gdx.utils.Null;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+
 public class HighscoreList {
     public final int  list_number = 10;
     public Highscore [] list;
-
+    Preferences prefs = Gdx.app.getPreferences("My Highscores");
     public Highscore[] getList(){
         return this.list;
     }
     public HighscoreList(){
         list = new Highscore[list_number];
+        for(int i =0; i<list_number;i++){
+            Highscore h = new Highscore();
+            h.setName(prefs.getString("Player".concat(Integer.toString(i))));
+            h.setScore(prefs.getInteger(Integer.toString(i)));
+            h.setDate(prefs.getString("Date".concat(Integer.toString(i))));
+            list[i] = h;
+        }
     }
     public HighscoreList(Highscore[] hs_l){
         list = new Highscore[list_number];
@@ -30,19 +45,42 @@ public class HighscoreList {
         }
         i=0;
     }
-    public void addHighscore(Highscore h) {
+    public int getIndex(Highscore highscore){
         int i = 0;
-        Highscore p=list[i];
-        Highscore p2=null;
-        while(list[i]!=null) {
-            if (list[i].getScore()< h.getScore()) {
-                p = list[i];
-                p2 = list[i + 1];
-                break;
-            }i++;
+        while (i<list_number) {
+            if(list[i].getScore()==highscore.getScore()&&list[i].getName()==highscore.getName()){
+                return i;
+            }
+            i++;
         }
-        list[i]=h;
-        list[i+1]=p;
-        list[i+2]=p2;
+        return 0;
+    }
+    public void printHighscoreList(){
+        int i = 0;
+        while(i<list_number){
+            System.out.print(list[i].getName());
+        }
+    }
+    public void insertHighscore(Highscore highscore) {
+            int i = 0;
+            Highscore p = null;
+            if(list[0]==null){
+                list[0]=highscore;
+                return;
+            }
+            while(i<list_number) {
+                if (list[i].getScore()< highscore.getScore()) {
+                    p = list[i];
+                    list[i]=highscore;
+                    return;
+                }i++;
+            }
+            if(i+1<list_number){
+                return;
+            }
+            while(i+1<list_number){
+                list[i+1]=list[i];
+                i++;
+            }
     }
 }
